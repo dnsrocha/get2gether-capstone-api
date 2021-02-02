d3df9767f53417ad43a42b5629a986b75d6aced4
dnsrocha <drocha@gmail.com>
2021-02-01 22:22:07 -0800

    ..

--- get2gether.py
+++ get2gether.py
@@ -1,9 +1,18 @@
-import time
 from flask import Flask
+#from dotenv import load_dotenv
+#load dotenv()
+
 
 app = Flask(__name__)
 app.debug=True
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+@app.route('/')
+def home():
+    return 'Get2Gether'
+
+@app.route('/search')
+def search():
+    return "search for contacts"
+
+if __name__ == "__main__":
+    app.run()
