18e28605befe28fb18f9b0aa91bc0ecaef690d2e
dnsrocha <drocha@gmail.com>
2021-02-02 21:18:41 -0800

    db initial setup

--- get2gether.py
+++ get2gether.py
@@ -1,18 +1,36 @@
-from flask import Flask
-#from dotenv import load_dotenv
-#load dotenv()
+from flask import Flask, request
+import os
+from pyrebase import pyrebase
+from dotenv import load_dotenv
+load dotenv()
 
+config = {
+    'apiKey': os.getenv('FLASK_APP_FIREBASE_API_KEY'),
+    'authDomain': os.getenv('FLASK_APP_FIREBASE_AUTH_DOMAIN'),
+    'databaseURL': os.getenv('FLASK_APP_FIREBASE_DATABASE_URL'),
+    'storageBucket': os.getenv('FLASK_APP_FIREBASE_STORAGE_BUCKET')
+}
 
 app = Flask(__name__)
+firebase = pyrebase.initialize_app(config)
 app.debug=True
 
 @app.route('/')
 def home():
     return 'Get2Gether'
 
-@app.route('/search')
+@app.route('/search', methods=['GET'])
 def search():
-    return "search for contacts"
+    # if request.method == 'GET':
+    # email = request.form['email']
+    # name = request.form['name']
+        return "search for contacts"
+    # else:
+    #     return ({'message': 'Error. Invalid endpoint.'})
+
+@app.route('/add_contacts', methods=['POST'])
+def add_contacts():
+    return "add contacts"
 
 if __name__ == "__main__":
     app.run()
