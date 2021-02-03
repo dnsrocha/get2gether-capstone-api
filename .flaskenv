6e41eca18c312ede6daf574daf3844d9df2fa7a7
dnsrocha <drocha@gmail.com>
2021-02-03 10:55:54 -0800

    cors setup, integration setup

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,2 @@
-FLASK_APP=main.py
+FLASK_APP=get2gether.py
 FLASK_ENV=development
--- get2gether.py
+++ get2gether.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 import os
 from pyrebase import pyrebase
+from flask_cors import CORS
 from dotenv import load_dotenv
-load dotenv()
+load_dotenv('.env')
+
 
 config = {
     'apiKey': os.getenv('FLASK_APP_FIREBASE_API_KEY'),
@@ -11,13 +13,21 @@ config = {
     'storageBucket': os.getenv('FLASK_APP_FIREBASE_STORAGE_BUCKET')
 }
 
+
 app = Flask(__name__)
 firebase = pyrebase.initialize_app(config)
+CORS(app)
 app.debug=True
 
 @app.route('/')
 def home():
     return 'Get2Gether'
+
+@app.route('/add_contacts', methods=['POST'])
+def add_contacts():
+    db = firebase.database()
+    # if request.method == 'POST':
+    return "add contacts"
 
 @app.route('/search', methods=['GET'])
 def search():
@@ -28,9 +38,11 @@ def search():
     # else:
     #     return ({'message': 'Error. Invalid endpoint.'})
 
-@app.route('/add_contacts', methods=['POST'])
-def add_contacts():
-    return "add contacts"
+
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
+
 
 if __name__ == "__main__":
     app.run()
